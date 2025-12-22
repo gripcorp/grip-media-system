9d29adb0189c6d58f240342617b4d1ff56d58776
dwshin-grip <dwshin@gripcorp.co>
2025-12-22 16:33:00 +0900

    [test] ivs connection check

--- src/main/kotlin/show/grip/mms/controller/HealthController.kt
+++ src/main/kotlin/show/grip/mms/controller/HealthController.kt
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Value
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import software.amazon.awssdk.services.ivs.IvsClient
 
 @RestController
 @RequestMapping("/monitor")
@@ -30,7 +31,9 @@ class HealthController (
     private val ivsRegion: String,
 
     @Value("\${aws.ivs.accountId}")
-    private val ivsAccountId: String
+    private val ivsAccountId: String,
+
+    private val ivsClient: IvsClient
 ){
 
     @GetMapping("/healthCheck")
@@ -48,5 +51,33 @@ class HealthController (
             "ivsRegion" to "$ivsRegion",
             "ivsAccountId" to "$ivsAccountId"
         )
+    }
+
+    @GetMapping("/ivscheck")
+    fun checkIvs(): Map<String, Any> {
+        val healthStatus = mutableMapOf<String, Any>(
+            "status" to "UP",
+            "service" to "grip-media-system",
+            "db-url" to dbUrl
+        )
+
+        // IVS 연결 확인
+        try {
+            // 가장 가볍게 1개만 조회해서 응답이 오는지 확인
+            val response = ivsClient.listChannels { it.maxResults(1) }
+            healthStatus["ivs"] = mapOf(
+                "status" to "CONNECTED",
+                "request_id" to response.responseMetadata().requestId()
+            )
+        } catch (e: Exception) {
+            healthStatus["ivs"] = mapOf(
+                "status" to "DISCONNECTED",
+                "error" to (e.message ?: "Unknown Error")
+            )
+            // 필요하다면 여기서 전체 status를 "DOWN"으로 변경할 수도 있습니다.
+            // healthStatus["status"] = "DOWN"
+        }
+
+        return healthStatus
     }
 }
